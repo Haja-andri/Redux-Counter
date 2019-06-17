a2ecdb7c77a88ccfab0f3dbaa01c5a9ba8196b83
Haja-andri <haja.andri@gmail.com>
2019-06-17 16:20:43 +0200

    Worked out Index.js (entry point)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import Counter from './components/Counter';
-import counter from './reducers';
+import { INCREMENT, DECREMENT, increment, decrement } from './actions';
 
-// This line instantiates our central Redux store.
-// The `createStore` function receives the reducer
-// that is responsible for updating the store, along
-// with any initial state that we may want the store
-// to start out with (which is none in this case).
-const store = createStore(counter);
+const initialState = {
+  count: 20
+}
 
-// Here, we wrap our main React component inside of
-// Provider tags, which come from the react-redux package.
-// This is needed because the store needs to know where it
-// is passing its state to. The Provider component is also
-// where the store "lives".
-ReactDOM.render(
-  <Provider store={store}>
-    <Counter />
-  </Provider>,
-  document.getElementById('root')
-);
+// Our reducer that handles our two action cases:
+// increment and decrement. It receives the state
+// of our redux store, along with an action created
+// by our action creator. What does the reducer
+// need to do with the count in each case?
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+    // Fill in the body of this case
+    return {
+      count: state.count + action.payload,
+    }
+    case DECREMENT:
+    // Fill in the body of this case
+    return {
+      count: state.count - action.payload,
+    }
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
